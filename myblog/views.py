3ed64675e407581ede6b49afc3ae35660cde6cf5
nagamasa634 <masahiro.nagasawa@dalab.jp>
2025-08-01 13:30:16 +0900

    アプデしました

--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import Postform
 from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
     return render(request, 'myblog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    return render(request, 'myblog/post_detail.html', {'post':post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = Postform(request.POST)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk = post.pk)
+    else:
+        form = Postform()
+    return render(request, 'myblog/post_edit.html', {'form':form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    if request.method == "POST":
+        form = Postform(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk = post.pk)
+    else:
+        form = Postform(instance=post)
+    return render(request, 'myblog/post_edit.html', {'form':form})
